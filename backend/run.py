2adaea7e8d41a347324e74c11f56a41bfde5439f
Stan <esanyumba@gmail.com>
2025-01-23 12:56:52 +0300

    update on cors

--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,9 @@
-from backend.app import create_app, db
+from .app import create_app, db
 from backend.app.models import User
 
 app = create_app()
 
-@app.shell_context_processors
+@app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User}
 
--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,2 +1,6 @@
-const API_URL = "https://anyumbake.tech"
+
+// Define the base URL for your Flask backend
+const API_URL = "http://localhost:5000";  // Flask server running locally
+
+// Export the URL for use in other files
 export default API_URL;
